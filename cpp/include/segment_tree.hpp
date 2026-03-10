74e20575b61389ec7270aebbaf1196c93852adf5
MeltingPPot <miaoting666666@163.com>
2026-03-10 21:48:20 +0800

    完成了算法库动态开点线段树的头文件：segment_tree.hpp

--- cpp/include/segment_tree.hpp
+++ cpp/include/segment_tree.hpp
@@ -8,58 +8,56 @@ namespace Malgolab{
     template<class T,class Combine=std::function<T(const T&,const T&)>>
     class SegmentTree{
         public:
-            void SegmentTree(cosnt vector<T> &data,T identity_,Combine combine_)
-            :n(data.size()),combine(combine_),identity(identity_)
-            {
-                t.resize(n);
-            }
-            void build(const vector<int> &data){
-                for(int i=0;i<data.size();++i){
-                    insert(1,0,n-1,i,data[i]);
+            SegmentTree(const std::vector<T> &data,T identity_,Combine combine_)
+            :n(data.size()),combine(combine_),identity(identity_){
+                root=NewPos();
+                for(int i=0;i<n;++i){
+                    insert(root,0,n-1,i,data[i]);
                 }
             }
             T query(int l,int r)const{
-                return query(1,0,n-1,l,r);
+                return query(root,0,n-1,l,r);
             }
             void modify(int x,T v){
-                insert(1,0,n-1,x,v);
+                insert(root,0,n-1,x,v);
             }
         private:
-        #define lson t[pos-1].ls
-        #define rson t[pos-1].rs
-            int n;
-            Combine combine;
-            T identity;
-            struct TREE{
+            struct Node{
                 int ls,rs;
                 T dat;
+                Node():ls(0),rs(0),dat(T()){}
             };
-            vector<TREE> t;
-            int NewNode(){
+            int n,root;
+            Combine combine;
+            T identity;
+            std::vector<Node> t;
+            int NewPos(){
                 t.emplace_back();
                 return t.size();
             }
             void push_up(int pos){
-                t[pos].dat=combine(t[lson].dat,t[rson].dat);
+                T lv=t[pos-1].ls?t[t[pos-1].ls-1].dat:identity;
+                T rv=t[pos-1].rs?t[t[pos-1].rs-1].dat:identity;
+                t[pos-1].dat=combine(lv,rv);
             }
-            void insert(int pos,int l,int r,int x,T val){
-                if(!pos) pos=NewNode();
-                if(l==x) return t[pos-1].dat=val,void();
+            void insert(int &pos,int l,int r,int x,T val){
+                if(!pos) pos=NewPos();
+                if(l==r) return t[pos-1].dat=val,void();
                 int mid((l+r)>>1);
-                if(x<=mid) insert(lson,l,mid);
-                else insert(rson,mid+1,r);
-                push_up(pos-1);
+                if(x<=mid) insert(t[pos-1].ls,l,mid,x,val);
+                else insert(t[pos-1].rs,mid+1,r,x,val);
+                push_up(pos);
             }
             T query(int pos,int l,int r,int ql,int qr)const{
                 if(!pos) return identity;
                 if(ql<=l&&r<=qr) return t[pos-1].dat;
                 int mid((l+r)>>1);
                 T ans(identity);
-                if(ql<=mid) ans=combine(ans,query(lson,l,mid,ql,qr));
-                if(mid<qr) ans=combine(ans,query(rson,mid+1,r,ql,qr));
+                if(ql<=mid) ans=combine(ans,query(t[pos-1].ls,l,mid,ql,qr));
+                if(mid<qr) ans=combine(ans,query(t[pos-1].rs,mid+1,r,ql,qr));
                 return ans;
             }
     };
 
-} // namespace algolib
+} // namespace Malgolab
 #endif
