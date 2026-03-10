1dd7b8469f5941e1810204b54a77f35407088c34
MeltingPPot <miaoting666666@163.com>
2026-03-10 23:17:30 +0800

    2完成了算法库线段树命令行文件：segment_cli_tree.cpp

--- cpp/include/segment_tree.hpp
+++ cpp/include/segment_tree.hpp
@@ -8,14 +8,18 @@ namespace Malgolab{
     template<class T,class Combine=std::function<T(const T&,const T&)>>
     class SegmentTree{
         public:
-            SegmentTree(const std::vector<T> &data,T identity_,Combine combine_)
+            SegmentTree(const std::vector<T> &data,
+                Combine combine_=std::plus<T>(),
+                T identity_=T())
             :n(data.size()),combine(combine_),identity(identity_){
                 root=NewPos();
+                t.reserve(n<<2);
                 for(int i=0;i<n;++i){
+                    // std::cout<<"CL:"<<data[i]<<std::endl;
                     insert(root,0,n-1,i,data[i]);
                 }
             }
-            T query(int l,int r)const{
+            T query(int l,int r){
                 return query(root,0,n-1,l,r);
             }
             void modify(int x,T v){
@@ -41,6 +45,7 @@ namespace Malgolab{
                 t[pos-1].dat=combine(lv,rv);
             }
             void insert(int &pos,int l,int r,int x,T val){
+                if(l>r) return;
                 if(!pos) pos=NewPos();
                 if(l==r) return t[pos-1].dat=val,void();
                 int mid((l+r)>>1);
@@ -48,7 +53,7 @@ namespace Malgolab{
                 else insert(t[pos-1].rs,mid+1,r,x,val);
                 push_up(pos);
             }
-            T query(int pos,int l,int r,int ql,int qr)const{
+            T query(int pos,int l,int r,int ql,int qr){
                 if(!pos) return identity;
                 if(ql<=l&&r<=qr) return t[pos-1].dat;
                 int mid((l+r)>>1);
