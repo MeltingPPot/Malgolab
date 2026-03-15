6ff85618053d0838e0cec565fa3705cff82c0787
MeltingPPot <miaoting666666@163.com>
2026-03-15 16:32:46 +0800

    实现了不同颜色的本地测评提示

--- test_judge.py
+++ test_judge.py
@@ -1,24 +1,25 @@
 import sys
 from pathlib import Path
+from colorama import init, Fore, Style
 
-# ss = Path(__file__).parent / 'python'
-# sys.path.insert(0, str(ss))
+# 初始化 colorama（自动适配 Windows）
+init(autoreset=True)
 
-from python.Malgolab.judge.local_judge import judge_one
+# 将 python 目录加入模块搜索路径
+ss = Path(__file__).parent / 'python'
+sys.path.insert(0, str(ss))
 
-GREEN = '\033[92m'
-RED = '\033[91m'
-RESET = '\033[0m'
-
-
+from Malgolab.judge.local_judge import judge_one
 
 src = Path("data/test_ab/sol.cpp")
 inp = Path("data/test_ab/1.in")
 ans = Path("data/test_ab/1.out")
 
 ok, msg = judge_one(src, inp, ans)
+
 if ok:
-    print(GREEN + msg + RESET)
+    # 绿色输出
+    print(Fore.GREEN + msg + Style.RESET_ALL)
 else:
-    print(RED + msg + RESET)
-# print(ok, msg)
+    # 红色输出
+    print(Fore.RED + msg + Style.RESET_ALL)
