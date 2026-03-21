e481407aad6c37bd05ecaade618a667f66921d88
MeltingPPot <miaoting666666@163.com>
2026-03-21 16:55:39 +0800

    增加默认接替代码模板

---
+++ templates/default.cpp
@@ -0,0 +1,11 @@
+// templates/default.cpp
+// $TITLE$
+// OJ: $OJ$, ID: $PID$
+// Date: $DATE$
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Your code here
+    return 0;
+}
