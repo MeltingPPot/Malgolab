ea25a801363b476dbb24decb94f40a3e8927b192
MeltingPPot <miaoting666666@163.com>
2026-03-25 12:19:45 +0800

    增添命令行主入口main.py

---
+++ python/Malgolab/cli/main.py
@@ -0,0 +1,14 @@
+# python/Malgolab/cli/main.py
+import click
+from .commands import init, fetch   # 确保 fetch 已导入
+
+@click.group()
+def cli():
+    """Malgolab - 个人算法竞赛训练平台"""
+    pass
+
+cli.add_command(init.init)
+cli.add_command(fetch.fetch)   # 添加
+
+if __name__ == '__main__':
+    cli()
